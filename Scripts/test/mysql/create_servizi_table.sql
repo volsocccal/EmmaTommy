5a44997a6fabaa248f1e7672287a16d1195fd0ed
fsaporito <sapo93@gmail.com>
2020-02-29 18:18:22 +0100

    Removed AUTO_INCREMENT

--- Scripts/test/mysql/create_servizi_table.sql
+++ Scripts/test/mysql/create_servizi_table.sql
@@ -1,12 +1,12 @@
-    CREATE DATABASE IF NOT EXISTS `servizi_db` ;
+    CREATE DATABASE IF NOT EXISTS servizi_db ;
     USE servizi_db;
 
-    DROP TABLE IF EXISTS `servizi`;
+    DROP TABLE IF EXISTS servizi;
 
-    CREATE TABLE `servizi` (
-    `codice_servizio` int(11) NOT NULL,
-    `codice_mezzo` varchar(45) DEFAULT NULL,
-    `km` int(11) DEFAULT NULL,
-    `email` varchar(45) DEFAULT NULL,
-    PRIMARY KEY (`codice_servizio`)
-    )ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1; 
+    CREATE TABLE servizi (
+    codice_servizio int(11) NOT NULL,
+    codice_mezzo varchar(45) DEFAULT NULL,
+    km int(11) DEFAULT NULL,
+    email varchar(45) DEFAULT NULL,
+    PRIMARY KEY (codice_servizio)
+    )ENGINE=InnoDB DEFAULT CHARSET=latin1; 
