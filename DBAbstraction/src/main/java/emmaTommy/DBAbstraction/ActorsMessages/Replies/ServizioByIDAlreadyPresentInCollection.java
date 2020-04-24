f30938f8f6a0e2d9e70f3ee0d62a8c1b5867711d
fsaporito <sapo93@gmail.com>
2020-04-25 00:02:22 +0200

    Refactored: Changed name and removed one collection name

---
+++ DBAbstraction/src/main/java/emmaTommy/DBAbstraction/ActorsMessages/Replies/ServizioByIDAlreadyPresentInCollection.java
@@ -0,0 +1,30 @@
+package emmaTommy.DBAbstraction.ActorsMessages.Replies;
+
+public class ServizioByIDAlreadyPresentInCollection extends DBOperationFaillure {
+	protected String ID;
+	protected String collectionName;
+	public ServizioByIDAlreadyPresentInCollection(String ID, String collectionName) {
+		super("Servizio is already present in the Collection");
+		if (ID == null) {
+			throw new NullPointerException("Received ID was null");
+		}
+		if (ID.isBlank()) {
+			throw new IllegalArgumentException("Received ID was blanck");
+		}
+		this.ID = ID;
+		if (collectionName == null) {
+			throw new NullPointerException("Received collectionName was null");
+		}
+		if (collectionName.isBlank()) {
+			throw new IllegalArgumentException("Received collectionName was blanck");
+		}
+		this.collectionName = collectionName;
+		this.cause = this.cause + collectionName;
+	}
+	public String getID() {
+		return this.ID;
+	}
+	public String GetCollectionName() {
+		return this.collectionName;
+	}
+}
