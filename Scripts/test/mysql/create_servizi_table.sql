8f3c180067ad0c17c01874fa8d9ce0dcda3665e1
fsaporito <sapo93@gmail.com>
2020-05-22 18:58:48 +0200

    Changed the PersistenceDB CollectionNames

--- Scripts/test/mysql/create_servizi_table.sql
+++ Scripts/test/mysql/create_servizi_table.sql
@@ -1,12 +1,58 @@
     CREATE DATABASE IF NOT EXISTS servizi_db ;
     USE servizi_db;
 
-    DROP TABLE IF EXISTS servizi;
-
-    CREATE TABLE servizi (
-    codice_servizio int(11) NOT NULL,
-    codice_mezzo varchar(45) DEFAULT NULL,
+    DROP TABLE IF EXISTS soreuAlpina;
+    CREATE TABLE soreuAlpina (
+    codice_servizio varchar(12) NOT NULL,
+    codice_mezzo varchar(45) NOT NULL,
     km int(11) DEFAULT NULL,
-    email varchar(45) DEFAULT NULL,
+    servizio_start_date DATE NOT NULL,
+    servizio_start_time TIME NOT NULL,
+    servizio_end_date DATE DEFAULT NULL,
+    servizio_end_time TIME DEFAULT NULL,
+    servizio_timestamp TIMESTAMP NOT NULL,
+    json BLOB NOT NULL,
+    PRIMARY KEY (codice_servizio)
+    )ENGINE=InnoDB DEFAULT CHARSET=latin1; 
+    
+    DROP TABLE IF EXISTS soreuLaghi;
+    CREATE TABLE soreuLaghi (
+    codice_servizio varchar(12) NOT NULL,
+    codice_mezzo varchar(45) NOT NULL,
+    km int(11) DEFAULT NULL,
+    servizio_start_date DATE NOT NULL,
+    servizio_start_time TIME NOT NULL,
+    servizio_end_date DATE DEFAULT NULL,
+    servizio_end_time TIME DEFAULT NULL,
+    servizio_timestamp TIMESTAMP NOT NULL,
+    json BLOB NOT NULL,
+    PRIMARY KEY (codice_servizio)
+    )ENGINE=InnoDB DEFAULT CHARSET=latin1; 
+    
+    DROP TABLE IF EXISTS soreuMetropolitana;
+    CREATE TABLE soreuMetropolitana (
+    codice_servizio varchar(12) NOT NULL,
+    codice_mezzo varchar(45) NOT NULL,
+    km int(11) DEFAULT NULL,
+    servizio_start_date DATE NOT NULL,
+    servizio_start_time TIME NOT NULL,
+    servizio_end_date DATE DEFAULT NULL,
+    servizio_end_time TIME DEFAULT NULL,
+    servizio_timestamp TIMESTAMP NOT NULL,
+    json BLOB NOT NULL,
+    PRIMARY KEY (codice_servizio)
+    )ENGINE=InnoDB DEFAULT CHARSET=latin1; 
+    
+    DROP TABLE IF EXISTS soreuPianura;
+    CREATE TABLE soreuPianura (
+    codice_servizio varchar(12) NOT NULL,
+    codice_mezzo varchar(45) NOT NULL,
+    km int(11) DEFAULT NULL,
+    servizio_start_date DATE NOT NULL,
+    servizio_start_time TIME NOT NULL,
+    servizio_end_date DATE DEFAULT NULL,
+    servizio_end_time TIME DEFAULT NULL,
+    servizio_timestamp TIMESTAMP NOT NULL,
+    json BLOB NOT NULL,
     PRIMARY KEY (codice_servizio)
     )ENGINE=InnoDB DEFAULT CHARSET=latin1; 
