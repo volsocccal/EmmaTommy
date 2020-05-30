c3c21f0e775ef4fa51915ab9f36c8bf7fd71fbde
fsaporito <sapo93@gmail.com>
2020-05-30 11:51:44 +0200

    Added equals() method for Servizio Object

--- DataModel/src/main/java/emmaTommy/TommyDataModel/Servizio.java
+++ DataModel/src/main/java/emmaTommy/TommyDataModel/Servizio.java
@@ -1,7 +1,6 @@
 package emmaTommy.TommyDataModel;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.ArrayList;
 
@@ -217,6 +216,143 @@ public class Servizio extends TommyDataModel {
 	public ArrayList<String> validateForStoring() {
 		ArrayList<String> errorMsgList = new ArrayList<String>();
 		return errorMsgList;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((assistiti == null) ? 0 : assistiti.hashCode());
+		result = prime * result + ((codice_servizio == null) ? 0 : codice_servizio.hashCode());
+		result = prime * result + km;
+		result = prime * result + ((luogo_arrivo == null) ? 0 : luogo_arrivo.hashCode());
+		result = prime * result + ((luogo_partenza == null) ? 0 : luogo_partenza.hashCode());
+		result = prime * result + ((missioneDate == null) ? 0 : missioneDate.hashCode());
+		result = prime * result + ((note == null) ? 0 : note.hashCode());
+		result = prime * result + ((orario_arrivo_ospedale == null) ? 0 : orario_arrivo_ospedale.hashCode());
+		result = prime * result + ((orario_arrivo_posto == null) ? 0 : orario_arrivo_posto.hashCode());
+		result = prime * result + ((orario_fine_servizio == null) ? 0 : orario_fine_servizio.hashCode());
+		result = prime * result + ((orario_inizio_servizio == null) ? 0 : orario_inizio_servizio.hashCode());
+		result = prime * result + ((orario_partenza_ospedale == null) ? 0 : orario_partenza_ospedale.hashCode());
+		result = prime * result + ((orario_partenza_posto == null) ? 0 : orario_partenza_posto.hashCode());
+		result = prime * result + ((squadra == null) ? 0 : squadra.hashCode());
+		result = prime * result + ((tag_idautomezzo == null) ? 0 : tag_idautomezzo.hashCode());
+		result = prime * result + ((tag_idintervento == null) ? 0 : tag_idintervento.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Servizio other = (Servizio) obj;
+		if (assistiti == null) {
+			if (other.assistiti != null)
+				return false;
+		} else if (assistiti.size() != other.assistiti.size()) {
+			return false;
+		} else {
+			for (int assistitoNum = 0; assistitoNum < assistiti.size(); assistitoNum++) {
+				Assistito assistito = assistiti.get(assistitoNum);
+				Assistito assistitoOther = other.assistiti.get(assistitoNum);
+				if (assistito == null) {
+					if (assistitoOther!= null)
+						return false;
+				} else {
+					if (!assistito.equals(assistitoOther))
+						return false;
+				}
+			}
+		}
+		if (codice_servizio == null) {
+			if (other.codice_servizio != null)
+				return false;
+		} else if (!codice_servizio.equals(other.codice_servizio))
+			return false;
+		if (km != other.km)
+			return false;
+		if (luogo_arrivo == null) {
+			if (other.luogo_arrivo != null)
+				return false;
+		} else if (!luogo_arrivo.equals(other.luogo_arrivo))
+			return false;
+		if (luogo_partenza == null) {
+			if (other.luogo_partenza != null)
+				return false;
+		} else if (!luogo_partenza.equals(other.luogo_partenza))
+			return false;
+		if (missioneDate == null) {
+			if (other.missioneDate != null)
+				return false;
+		} else if (!missioneDate.equals(other.missioneDate))
+			return false;
+		if (note == null) {
+			if (other.note != null)
+				return false;
+		} else if (!note.equals(other.note))
+			return false;
+		if (orario_arrivo_ospedale == null) {
+			if (other.orario_arrivo_ospedale != null)
+				return false;
+		} else if (!orario_arrivo_ospedale.equals(other.orario_arrivo_ospedale))
+			return false;
+		if (orario_arrivo_posto == null) {
+			if (other.orario_arrivo_posto != null)
+				return false;
+		} else if (!orario_arrivo_posto.equals(other.orario_arrivo_posto))
+			return false;
+		if (orario_fine_servizio == null) {
+			if (other.orario_fine_servizio != null)
+				return false;
+		} else if (!orario_fine_servizio.equals(other.orario_fine_servizio))
+			return false;
+		if (orario_inizio_servizio == null) {
+			if (other.orario_inizio_servizio != null)
+				return false;
+		} else if (!orario_inizio_servizio.equals(other.orario_inizio_servizio))
+			return false;
+		if (orario_partenza_ospedale == null) {
+			if (other.orario_partenza_ospedale != null)
+				return false;
+		} else if (!orario_partenza_ospedale.equals(other.orario_partenza_ospedale))
+			return false;
+		if (orario_partenza_posto == null) {
+			if (other.orario_partenza_posto != null)
+				return false;
+		} else if (!orario_partenza_posto.equals(other.orario_partenza_posto))
+			return false;
+		if (squadra == null) {
+			if (other.squadra != null)
+				return false;
+		} else if (squadra.size() != other.squadra.size()) {
+			return false;
+		} else {
+			for (int membroNum = 0; membroNum < squadra.size(); membroNum++) {
+				Membro membro = squadra.get(membroNum);
+				Membro membroOther = other.squadra.get(membroNum);
+				if (membro == null) {
+					if (membroOther != null)
+						return false;
+				} else {
+					if (!membro.equals(membroOther)) 
+						return false;
+				}
+			}
+		}
+		if (tag_idautomezzo == null) {
+			if (other.tag_idautomezzo != null)
+				return false;
+		} else if (!tag_idautomezzo.equals(other.tag_idautomezzo))
+			return false;
+		if (tag_idintervento == null) {
+			if (other.tag_idintervento != null)
+				return false;
+		} else if (!tag_idintervento.equals(other.tag_idintervento))
+			return false;
+		return true;
 	}
 	
 	
