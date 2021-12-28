930a1a00e5b2b7825f1a51e40cbd89d4814ef17a
fsaporito <sap93@gmail.com>
2021-12-28 13:50:29 +0100

    Add launch script

---
+++ EmmaTommySupervisor/scripts/launchEmmaTommy.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+cd ..
+
+java -jar EmmaTommySupervisor-0.0.1-SNAPSHOT-shaded.jar
+
+
+#java -jar EmmaTommySupervisor-0.0.1-SNAPSHOT-shaded.jar conf
+
+#java -jar EmmaTommySupervisor-0.0.1-SNAPSHOT-shaded.jar /media/sapo93/DATI/GitHub/VolCal/EmmaTommy/EmmaTommySupervisor/target/conf
