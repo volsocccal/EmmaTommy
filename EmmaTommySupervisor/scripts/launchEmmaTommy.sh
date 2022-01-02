ecdf84fdd9e46c4adaec1ec2860be3f158de8726
fsaporito <sap93@gmail.com>
2022-01-02 16:35:51 +0100

    Add function for creating empty folders before launching the jar

--- EmmaTommySupervisor/scripts/launchEmmaTommy.sh
+++ EmmaTommySupervisor/scripts/launchEmmaTommy.sh
@@ -1,6 +1,27 @@
 #!bin/bash
 cd ..
 
+function delete_and_create()
+{
+    if [ -d "$1" ]; then
+        echo "$1 exists"
+        cd "$1"
+        rm -fr *
+        cd ..
+    else
+        echo "$1 doesn't exists"
+    fi
+    mkdir -p "$1"
+}
+
+delete_and_create "log"
+delete_and_create "db"
+delete_and_create "data_json_missioni_download"
+delete_and_create "data_json_servizi_download"
+delete_and_create "data_xml_download"
+delete_and_create "post_json_folder"
+
+
 java -jar EmmaTommySupervisor-0.0.1-SNAPSHOT-shaded.jar
 
 
